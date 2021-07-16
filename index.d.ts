662a17628a0ac2f5b2eeb001cf051deb14cfd59a
listanegra <vitormac3@hotmail.com>
2021-07-15 21:05:17 -0300

    Accident domain type definition

---
+++ index.d.ts
@@ -0,0 +1,69 @@
+declare namespace road.domain {
+
+    export interface Accident { 
+
+        _id?: import('bson').ObjectId;
+
+        br: string;
+
+        causa_acidente: string;
+
+        classificacao_acidente: string;
+
+        condicao_metereologica: string;
+
+        data: Date;
+
+        delegacia: string;
+
+        dia_semana: string;
+
+        fase_dia: string;
+
+        feridos: string;
+
+        feridos_graves: string;
+
+        feridos_leves: string;
+
+        horario: string;
+
+        id_prf: string;
+
+        ignorados: string;
+
+        ilesos: string;
+
+        km: string;
+
+        latitude: number;
+
+        longitude: number;
+
+        mortos: string;
+
+        municipio: string;
+
+        pessoas: string;
+
+        regional: string;
+
+        sentido_via: string;
+
+        tipo_acidente: string;
+
+        tipo_pista: string;
+
+        tracado_via: string;
+
+        uf: string;
+
+        uop: string;
+
+        uso_solo: string;
+
+        veiculos: string;
+
+    }
+
+}
